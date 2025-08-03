ea0df57f51a02d08f8d5022332eb6f64c07c40f5
Максим <max.andr2005@yandex.ru>
2025-08-03 22:21:08 +0300

    Добавлена строка состояния содержащая версию Версионирование начато с alpha-0.1.0

--- ViewModels/MicroViewModel.cs
+++ ViewModels/MicroViewModel.cs
@@ -1,14 +1,17 @@
-﻿using Micro.Models;
+﻿using Micro.Infrastructure.Entrys;
+using Micro.Models;
 using Micro.Resources;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Deployment.Internal;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Micro.Infrastructure.Entrys;
 
 
 namespace Micro.ViewModels
@@ -16,18 +19,19 @@ namespace Micro.ViewModels
     internal class MicroViewModel : INotifyPropertyChanged
     {
 
-        public CpuState CpuState { get; }
-
+        #region ViewModels
         public CpuViewModel CpuVm { get; }
         public MicroProgramMemoryViewModel MicroprogramMemoryVm { get; }
         public RamViewModel RamVm { get; }
         public RegistersViewModel RegistersVm { get; }
         public AddressTranslationViewModel AddressTranslationVm { get; }
-
+        #endregion
+        public CpuState CpuState { get; }
+        public ObservableCollection<RegisterEntry> Registers { get; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<RegisterEntry> Registers { get; }
+        public string Version => FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).ProductVersion;
 
 
         public MicroViewModel()
